0d89e2f1fbfa3cb3f3ba021c665ee31eb54c5bfb
sunny-5575 <141895999+sunny-5575@users.noreply.github.com>
2023-08-14 18:44:11 +0530

    Update main.js

--- Demo/main.js
+++ Demo/main.js
@@ -1,5 +1,5 @@
 import * as THREE from 'https://unpkg.com/three@^0.155.0/build/three.module.js'
-import { GLTFLoader } from 'three/loaders/GLTFLoader.js'
+import { GLTFLoader } from './Demo/GLTFLoader.js'
 import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls'
 import gsap from 'gsap'
 
