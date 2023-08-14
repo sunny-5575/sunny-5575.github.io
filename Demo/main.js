691458f6c561a031a8ee3a4dfd74916b04e9867b
sunny-5575 <141895999+sunny-5575@users.noreply.github.com>
2023-08-14 18:22:04 +0530

    Update main.js

--- Demo/main.js
+++ Demo/main.js
@@ -1,5 +1,5 @@
 import * as THREE from 'https://unpkg.com/three@^0.155.0/build/three.module.js'
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+import { GLTFLoader } from 'three/loaders/GLTFLoader.js'
 import { OrbitControls } from 'three/addons/examples/jsm/controls/OrbitControls.js'
 import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls'
 import gsap from 'gsap'
